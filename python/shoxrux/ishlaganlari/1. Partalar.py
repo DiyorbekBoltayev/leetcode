57f77b91aa285787daa19a49fddf884852695325
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-11-06 20:19:54 +0500

    shoxrux

--- python/shoxrux/ishlaganlari/1. Partalar.py
+++ python/shoxrux/ishlaganlari/1. Partalar.py
@@ -1,16 +1,20 @@
-a=input()
-s=a.split()
-d=0
+a = input()
+s = a.split()
+d = 0
+
+
 def juft(j):
-    j=int(j)
-    if j%2==0:
+    j = int(j)
+    if j % 2 == 0:
         return True
-    else: 
-        return False
-for i in s:
-    i=int(i)
-    if juft(i):
-        d+=i/2
     else:
-        d+=(i+1)/2
+        return False
+
+
+for i in s:
+    i = int(i)
+    if juft(i):
+        d += i / 2
+    else:
+        d += (i + 1) / 2
 print(int(d))
